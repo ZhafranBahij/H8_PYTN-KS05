9be5f05a2e5f959b508be736bae8b84923a6490f
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2022-03-07 16:32:27 +0700

    first commit

---
+++ P-03/mod.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+s = "Hacktiv8-PTP Python for Data Science"
+a = [100, 200, 300]
+
+def foo(arg):
+    print(arg)
+    
+class Foo:
+    pass
+
+# if (__name__ == "main):
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Introduce to Python Data Science
+
+Nama Program : Introduce to Python Data Science
+batch : 05
+Nama saya : M. Zhafran Bahij
+
+Setiap folder mewakili setiap pertemuan
